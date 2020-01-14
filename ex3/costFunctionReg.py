a6b091cd1834b87afb238eecc2db780b709ac367
Closedboy <zijian_huang@sjtu.edu.cn>
2020-01-14 18:03:27 +0800

    add costFunctionReg.py

---
+++ ex3/costFunctionReg.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : Zijian Huang
+@Contact : zijian_huang@sjtu.edu.cn
+@FileName: costFunctionReg.py
+@Time    : 20-1-10 下午1:27
+"""
+import numpy as np
+from sigmoid import sigmoid
+
+
+def costFunctionReg(theta, X, y, reg_factor, requires_grad=False):
+    epsilon = 1e-5
+    m = np.size(X, axis=0)
+    h = sigmoid(X @ theta)
+    J = -1.0 / m * (y.T @ np.log(h + epsilon) + (1 - y).T @ np.log(1 - h + epsilon)) \
+        + reg_factor / (2.0 * m) * theta[1:].T @ theta[1:]
+    if requires_grad:
+        temp = theta
+        temp[0, :] = 0
+        grad = 1.0 / m * X.T @ (h - y) + reg_factor / m * temp
+        return J, grad
+    else:
+        return J
+
